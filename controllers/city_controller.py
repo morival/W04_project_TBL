3b3a400f62f14f1ce9d1795feeb819f5c64c1eb9
Mateusz <mateusz.harlos@gmail.com>
2020-11-07 17:57:36 +0000

    added templates for cities

---
+++ controllers/city_controller.py
@@ -0,0 +1,10 @@
+from flask import Flask, render_template, request, redirect, Blueprint
+from models.city import City
+import repositories.city_repository as city_repository
+
+cities_blueprint = Blueprint("cities", __name__)
+
+@cities_blueprint.route("/cities")
+def cities():
+    cities = city_repository.select_all()
+    return render_template("cities/index.html", cities=cities)
